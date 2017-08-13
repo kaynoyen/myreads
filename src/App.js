a7f3e5f16d909305f425537aa63af3546f1fb70d
Kay Noyen <kaynoyen@gmail.com>
2017-08-13 16:32:49 +0200

    feat: Add books as state to BooksApp and pull initial books from API

--- src/App.js
+++ src/App.js
@@ -7,9 +7,20 @@ import SearchPage from './SearchPage'
 
 class BooksApp extends React.Component {
 
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books})
+    })
+  }
+
   render() {
     return (
       <div className="app">
+
         <Route exact path="/" render={() => (
           <MainPage/>
         )}/>
