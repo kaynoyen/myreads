d188501e980ff5febe277df1104ace799d802ccc
Kay Noyen <kaynoyen@gmail.com>
2017-08-13 16:58:31 +0200

    feat: add ListBooks basic structure and react router search button

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class ListBooks extends Component {
 
   render() {
 
     return(
-    	<div>LIST BOOKS</div>
+    	<div className="list-books">
+    		<div className="list-books-title">
+    			<h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+            	<div>
+            	</div>
+            	<div className="open-search">
+            		<Link className="close-search" to="/search">Add a book</Link>
+            	</div>
+            </div>
+        </div>
     )
   }
 }
