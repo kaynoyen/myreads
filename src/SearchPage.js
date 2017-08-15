a8df6b2c48d69fc701255c9f0c669f862e196be8
Kay Noyen <kaynoyen@gmail.com>
2017-08-15 20:16:02 +0200

    Add query state to searchPage

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Book from './Book'
 
 class SearchPage extends Component {
 
+	state = {
+		query: ''
+	} 
+
+	updateQuery = (query) => {
+		this.setState({
+			query: query.trim()
+		})
+	}
+
+	clearQuery = () => {
+		this.setState({
+			query: ''
+		})
+	}
+
 render() {
+
+	const {query} = this.state
+
+	let showingBooks
 
 	return(
 
@@ -11,16 +32,7 @@ render() {
        		<div className="search-books-bar">
        			<Link className="close-search" to="/">Close</Link>
               <div className="search-books-input-wrapper">
-                {/* 
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                 <input type="text" placeholder="Search by title or author"/>
-                
               </div>
             </div>
             <div className="search-books-results">
